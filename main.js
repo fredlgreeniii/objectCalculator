eeeee6f6b9c4264948fde11883e060a17af49784
fredlgreeniii <fgreen@pivottechschool.com>
2020-08-13 17:09:38 -0500

    Update main.js
    Fred Updates to the Calculator

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 let calculator = {
     // ... your code ...
+    sum() {
+      return this.x + this.y;
+    },
+  
+    mul() {
+      return this.x * this.y;
+    },
+  
+    read() {
+      this.x = +prompt('x?', 0);
+      this.y = +prompt('y?', 0);
+    }
   };
   
   calculator.read();
